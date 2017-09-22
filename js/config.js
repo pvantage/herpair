54ab1531239aa0259ff5968ccd0ec2aebd4a72f1
user <priyanka.vantage123@gmail.com>
2017-09-22 17:30:01 +0530

    Initial uploades

---
+++ js/config.js
@@ -0,0 +1,21 @@
+var siteurl = "http://herpairapp.vantagewebtech.com//index.php";
+
+function gup(sParam) {
+  var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for(i = 0; i < sURLVariables.length; i++){
+        sParameterName = sURLVariables[i].split('=');
+
+        if(sParameterName[0] === sParam){
+            return sParameterName[1] === undefined ? true : sParameterName[1];
+        }
+    }
+}
+
+function gotoBack(){
+	history.go(-1);
+    navigator.app.backHistory();
+}
